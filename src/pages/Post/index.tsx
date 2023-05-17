e2d4778c1c432e1197837b1a5c0bd2a7957f4e18
Bruno Santos <brsantos197@gmail.com>
2023-05-17 11:18:57 -0300

    code highlights

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,3 +1,5 @@
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+
 import { useEffect, useContext, useState } from 'react'
 import { useParams } from "react-router-dom";
 import { PostInfo } from "./components/PostInfo";
@@ -5,11 +7,15 @@ import { api } from '../../lib/axios';
 import { GithubContext } from '../../contexts/GithubContext';
 import { PostType } from '../../@types/post.type';
 import { PostContent } from './styles';
+import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { useTheme } from 'styled-components';
 
 export const Post = () => {
   const { postId } = useParams()
   const { user, repo } = useContext(GithubContext)
   const [post, setPost] = useState<PostType>()
+
+  const { COLORS } = useTheme()
 
   useEffect(() => {
     const getPost = async () => {
@@ -25,10 +31,37 @@ export const Post = () => {
     <main>
       {post ? (
         <>
+
           <PostInfo post={post} />
-          <PostContent>
-            {post.body}
-          </PostContent>
+          <PostContent
+            children={post.body}
+            components={{
+              code({ children, className, ...props }) {
+                const match = /language-(\w+)/.exec(className || "");
+
+                if (!match) {
+                  return <code className={className} {...props} />
+                }
+
+                return (
+                  <SyntaxHighlighter language={match[1]} style={{
+                    ...dracula,
+                    'code[class*="language-"]': {
+                      ...dracula['code[class*="language-"]'],
+                      backgroundColor: COLORS['base-post'],
+                      color: COLORS['base-text'],
+                    },
+                    'pre[class*="language-"]': {
+                      ...dracula['pre[class*="language-"]'],
+                      backgroundColor: COLORS['base-post'],
+                      color: COLORS['base-text'],
+                    },
+                  }}>
+                    {children.toString()}
+                  </SyntaxHighlighter>
+                )
+              }
+            }} />
         </>
       ) : null}
     </main>
